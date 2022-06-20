29e4339fa9c19387ea3780cc45992f9c8ed2293c
Pongsak Sanguanwong <9073192+bemillenium@users.noreply.github.com>
2022-06-20 16:12:42 +0700

    feat: option for auto assignee

--- .github/workflows/upload-to-dojo.py
+++ .github/workflows/upload-to-dojo.py
@@ -1,42 +1,46 @@
 import requests
-import json
 from datetime import datetime
 import configparser
 import json
 
-def find_product_by_name(host,api_key,product_name):
+
+def find_product_by_name(host, api_key, product_name):
     headers = dict()
     AUTH_TOKEN = "Token " + str(api_key)
     headers['Authorization'] = AUTH_TOKEN
     print("\n==============Lists Products=================")
-    r = requests.get(host + "/products/?name="+str(product_name), headers=headers, verify=True)
+    r = requests.get(host + "/products/?name=" + str(product_name), headers=headers, verify=True)
     print(r.text)
 
     r = json.loads(r.text)
-    if(r['count'] > 0):
+    if (r['count'] > 0):
         return r['results']
     else:
         return None
 
-def find_engagement(host,api_key,engagement_name,product_id,engagement_status):
+
+def find_engagement(host, api_key, engagement_name, product_id, engagement_status):
     headers = dict()
     AUTH_TOKEN = "Token " + str(api_key)
     headers['Authorization'] = AUTH_TOKEN
     print("\n==============Lists Engagement=================")
-    r = requests.get(host + "/engagements/?name="+str(engagement_name)+'&product='+str(product_id)+'&status='+str(engagement_status), headers=headers, verify=True)
+    r = requests.get(
+        host + "/engagements/?name=" + str(engagement_name) + '&product=' + str(product_id) + '&status=' + str(
+            engagement_status), headers=headers, verify=True)
     print(r.text)
 
     r = json.loads(r.text)
-    if(r['count'] > 0):
+    if (r['count'] > 0):
         return r['results']
     else:
         return None
 
-def create_product(host,api_key,product_name,product_type,description):
+
+def create_product(host, api_key, product_name, product_type, description):
     headers = dict()
     json = dict()
 
-    AUTH_TOKEN = "Token "+str(api_key)
+    AUTH_TOKEN = "Token " + str(api_key)
     headers['Authorization'] = AUTH_TOKEN
     headers['content-type'] = "application/json"
     print(headers)
@@ -45,13 +49,14 @@ def create_product(host,api_key,product_name,product_type,description):
     json['name'] = str(product_name)
     json['description'] = str(description)
 
-    r = requests.post(host+"/products/", headers=headers, verify=True, json=json)
+    r = requests.post(host + "/products/", headers=headers, verify=True, json=json)
     print(r)
     print(r.text)
 
     return r.status_code, r.text
 
-def create_engagement(host,api_key,name,product_id,commit_hash,branch_tag,source_code_management_uri,lead):
+
+def create_engagement(host, api_key, name, product_id, commit_hash, branch_tag, source_code_management_uri, lead):
     print("\n==============Create Engagement================")
     headers = dict()
     json = dict()
@@ -76,12 +81,13 @@ def create_engagement(host,api_key,name,product_id,commit_hash,branch_tag,source
     json['lead'] = lead
 
     print(json)
-    r = requests.post(host+"/engagements/", headers=headers, verify=True, json=json)
+    r = requests.post(host + "/engagements/", headers=headers, verify=True, json=json)
     print(r)
     print(r.text)
     return r.status_code, r.text
 
-def upload_scan_result(host,api_key,product_name,engagement_name,scan_type,file_path):
+
+def upload_scan_result(host, api_key, product_name, engagement_name, scan_type, file_path):
     print("\n===============Upload Scan Results============")
     headers = dict()
 
@@ -90,7 +96,7 @@ def upload_scan_result(host,api_key,product_name,engagement_name,scan_type,file_
     # print(headers)
 
     files = dict()
-    files['file'] = open(file_path,'rb')
+    files['file'] = open(file_path, 'rb')
 
     json = dict()
     json['minimum_severity'] = "Info"
@@ -102,12 +108,13 @@ def upload_scan_result(host,api_key,product_name,engagement_name,scan_type,file_
     json['product_name'] = product_name
     json['scan_type'] = scan_type
 
-    r = requests.post(host+"/import-scan/", headers=headers, verify=True, data=json, files=files)
+    r = requests.post(host + "/import-scan/", headers=headers, verify=True, data=json, files=files)
     print(r)
     print(r.text)
     return r.status_code, r.text
 
-def reimport_scan_result(host,api_key,product_name,engagement_name,scan_type,file_path):
+
+def reimport_scan_result(host, api_key, product_name, engagement_name, scan_type, file_path):
     print("\n===============Re-import Scan Results============")
     headers = dict()
 
@@ -133,31 +140,22 @@ def reimport_scan_result(host,api_key,product_name,engagement_name,scan_type,fil
     print(r.text)
     return r.status_code, r.text
 
-def find_user_id_from_file(host, api_key, product_name, repo_info_file_path):
-    f = open(repo_info_file_path)
-    data = json.load(f)
-    f.close()
-    email = None
 
-    if product_name in data:
-        print("Key exists")
-        x = data[product_name]
-        email = data[product_name]['owner']
-    # print(email)
-
+def find_user_id_from_email(host, api_key, email):
     headers = dict()
     AUTH_TOKEN = "Token " + str(api_key)
     headers['Authorization'] = AUTH_TOKEN
 
     print("\n==============List Users=================")
-    r = requests.get(host + "/users/?email="+str(email), headers=headers, verify=True)
+    r = requests.get(host + "/users/?email=" + str(email), headers=headers, verify=True)
     # print(r.text)
 
     r = json.loads(r.text)
-    if(r['count'] > 0):
+    if (r['count'] > 0):
         return r['results']
     else:
         return None
+
 
 print("starting")
 product_id = None
@@ -186,33 +184,38 @@ try:
 except:
     reupload_enabled = "false"
 
-query_result = find_product_by_name(url,api_key,product_name)
+
+auto_assign_enabled = config['notify']['auto_assign_enabled']
+# Get email to assignee
+if auto_assign_enabled == "true":
+    email = config['notify']['assignee']
+else:
+    email = None
+
+query_result = find_product_by_name(url, api_key, product_name)
 if (query_result != None):
     print("Product is created already")
     product_id = query_result[0]['id']
 else:
     print("Creating new Product")
-    status_code, result = create_product(url,api_key,product_name,product_type,description)
+    status_code, result = create_product(url, api_key, product_name, product_type, description)
     result = json.loads(result)
     product_id = result['id']
 print(product_id)
 
-
 # Don't create the existing engagement
 engagement_name_id = None
-query_result = find_engagement(url,api_key,engagement_name,product_id,'In Progress')
+query_result = find_engagement(url, api_key, engagement_name, product_id, 'In Progress')
 print(query_result)
 
 # find user to assign engagement
 user_id = None
-user_data = find_user_id_from_file(url,api_key,product_name,"repo_info.json")
+user_data = find_user_id_from_email(url, api_key, email)
 if user_data != None:
-    # print(user_data)
     user_id = user_data[0]['id']
 else:
     user_id = 1
 print(user_id)
-
 
 # found engagement and try to reimport
 if query_result is not None and reupload_enabled == 'true':
@@ -222,8 +225,9 @@ if query_result is not None and reupload_enabled == 'true':
     status_code, result = reimport_scan_result(url, api_key, product_name, engagement_name, scan_type, file_path)
 else:
     # not found and engagement or force to create a new engagement
-    status_code, result = create_engagement(url,api_key,engagement_name,product_id,commit_hash,branch,source_code_management_uri,user_id)
+    status_code, result = create_engagement(url, api_key, engagement_name, product_id, commit_hash, branch,
+                                            source_code_management_uri, user_id)
     result = json.loads(result)
     engagement_id = result['id']
     print(engagement_id)
-    status_code, result = upload_scan_result(url,api_key,product_name,engagement_name,scan_type,file_path)
+    status_code, result = upload_scan_result(url, api_key, product_name, engagement_name, scan_type, file_path)
